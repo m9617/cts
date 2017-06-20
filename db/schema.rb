993aa6236de5773994ca2996fc9c9ef5e76aeed4
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 15:40:02 +0000

    Created seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615173140) do
+ActiveRecord::Schema.define(version: 20170615170049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170615173140) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "portfolios", force: :cascade do |t|
+ create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
     t.text     "body"
@@ -31,12 +31,12 @@ ActiveRecord::Schema.define(version: 20170615173140) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
-  create_table "skills", force: :cascade do |t|
+  
+create_table "skills", force: :cascade do |t|
     t.string   "title"
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
-
+  
 end
